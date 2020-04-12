4669198a7739d9851ff7aba069b20d7d16e38b39
Lucas Menendez <epucas@gmail.com>
2020-04-12 02:41:16 +0200

    better form performance

--- js/business-form.js
+++ js/business-form.js
@@ -1,90 +1,97 @@
-function onChangeStep(callback) {
-    let prevs = $('.step-control.prev');
-    prevs.on('click', event => {
-        let following = $(event.target).attr("data-prev");
-        callback(following);
+/**
+ * Question validators 
+ */
+
+function optionsValidator(question) {
+    let element = $(`.question[data-question="${question}"]`);
+    let options = element.find("input[type=\"radio\"], .option-input");
+
+    options.on("input", e => {
+        let value = $(e.target).val() || $(e.target).text();
+
+        if (value !== "") {
+            if (value === "other") {
+                element.removeClass("completed");
+                let input = element.find(".option-input");
+                input.empty().attr("contenteditable", "true").focus();
+            } else element.addClass("completed");
+        } else element.removeClass("completed");
     });
-    
-    let nexts = $('.step-control.next');
-    nexts.on('click', () => {
-        let disabled = $(event.target).hasClass("disabled");
-        if (!disabled) {
-            let following = $(event.target).attr("data-next");
-            callback(following);
-        }
+}
+
+function selectionValidator(question) {
+    let element = $(`.question[data-question="${question}"]`);
+    let options = element.find("input[type=\"checkbox\"]");
+
+    let validated = $(options).is(":checked");
+    if (validated) element.addClass("completed");
+    else element.removeClass("completed");
+
+
+    options.on("input", () => {
+        validated = $(options).is(":checked");
+
+        if (validated) element.addClass("completed");
+        else element.removeClass("completed");
     });
 }
 
-function onOptionSelected(question, callback) {
-    let path = `[data-question="${question}"] .hidden-input`;
-    let siblings = document.querySelectorAll(path);
-    let filled = Array.of(...siblings).some(input => input.checked);
-    
-    $(path).on("change", event => {
-        let value = $(event.target).val();
+function numberValidator(question) {
+    let element = $(`.question[data-question="${question}"]`);
+    let input = element.find("input.user-input");
 
-        if (value === "other") {
-            onInputUser(question, completed => callback(completed));
-        } else {
-            restoreUserInput(question);
-            if (value !== "") callback(true);
-            else callback(false);
-        }
+    input.on("input", e => {
+        let value = $(e.target).val();
+        if (value === "property") value = 0; 
+
+        if (value !== "" && parseFloat(value) >= 0) element.addClass("completed");
+        else element.removeClass("completed");
     });
-
-    callback(filled);
 }
 
-function onFieldFilled(question, callback) {
-    let path = `[data-question="${question}"] input`
-    let siblings = document.querySelectorAll(path);
-    let filled = Array.of(...siblings).some(input => input.value && input.value !== "");
+function emailValidator(question) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let element = $(`.question[data-question="${question}"]`);
+    let input = element.find("input[type=\"email\"]");
 
-    $(path).on("input", () => {
-        filled = Array.of(...siblings).some(input => input.value && input.value !== "");
-        callback(filled);
+    input.on("input", e => {
+        let value = $(e.target).val();
+
+        if (re.test(value.toLowerCase())) element.addClass("completed");
+        else element.removeClass("completed");
     });
-    callback(filled);
 }
 
-function setQuestionEnabled(question, enabled) {
-    let questionElement = $(`[data-question="${question}"]`);
-    if (enabled) questionElement.removeClass("disabled");
-    else questionElement.addClass("disabled");
+/**
+ * Question enablers & disablers 
+ */
+
+function enableQuestion(question) {
+    $(`[data-question="${question}"]`).removeClass("disabled");
 }
 
-function onInputUser(question, callback) {
-    let input = $(`[data-question="${question}"] .option-input`);
-    input.empty();
-    input.attr("contenteditable", "true");
-    input.on("input", () => {
-        if (input.text() !== "") callback(true);
-        else callback(false);
-    });
-    callback(false);
-}
-
-function restoreUserInput(question) {
-    let input = $(`[data-question="${question}"] .option-input`);
-    input.attr("contenteditable", "false");
-    input.text("Otro");
-}
-
-function allowsToContinue(step, enabled) {
-    let controls = $(`.step[data-step="${step}"] .step-control.next`);
-    if (enabled) controls.removeClass("disabled");
-    else controls.addClass("disabled");
-}
-
-function updateProgress(next) {
-    let steps = {
-        1: "7%",
-        2: "50%",
-        3: "100%"
+function disableQuestionsFrom(first, last = 6) {
+    for (let i = first; i <= last; i++) {
+        let questionElement = $(`[data-question="${i}"]`);
+        questionElement.addClass("disabled");
     }
-
-    $(".form-progress .steps-indicator .progress-bar").css("width", steps[next]);
 }
+
+/**
+ * Step control enablers & disablers 
+ */
+
+function enableStepControls(step) {
+    $(`.step[data-step="${step}"] .step-control.next`).removeClass("disabled");
+}
+
+function disableStepControls(step) {
+    $(`.step[data-step="${step}"] .step-control.next`).addClass("disabled");
+}
+
+/**
+ * Step enablers & disablers 
+ */
 
 function showNextStep(next) {
     let current = $(".step").not(".hidden");
@@ -96,49 +103,109 @@ function showNextStep(next) {
             following.removeClass("hidden")
         });
     });
+
+    setProgressBar(next);
+}
+
+function setProgressBar(next) {
+    let steps = {
+        1: "7%",
+        2: "50%",
+        3: "100%"
+    }
+
+    $(".form-progress .steps-indicator .progress-bar").css("width", steps[next]);
+}
+
+/**
+ * Checkers
+ */
+
+function isQuestionCompleted(question) {
+    return $(`.question[data-question="${question}"]`).hasClass("completed");
+}
+
+function checkStepOne() {
+    if (isQuestionCompleted(1)) {
+        enableQuestion(2);
+        if (isQuestionCompleted(2)) {
+            enableQuestion(3);
+            if (isQuestionCompleted(3)) enableStepControls(1);
+            else disableStepControls(1);
+        } else {
+            disableQuestionsFrom(3);
+            disableStepControls(1);
+        }
+    } else {
+        disableQuestionsFrom(2);
+        disableStepControls(1);
+    }
+
+    return isQuestionCompleted(1) && isQuestionCompleted(2) && isQuestionCompleted(3);
+}
+
+function checkStepTwo() {
+    if (isQuestionCompleted(4)) {
+        enableQuestion(5);
+        if (isQuestionCompleted(5)) {
+            enableQuestion(6);
+            if (isQuestionCompleted(6)) enableStepControls(2);
+            else disableStepControls(2);
+        } else {
+            disableQuestionsFrom(6);
+            disableStepControls(2);
+        }
+    } else {
+        disableQuestionsFrom(5);
+        disableStepControls(2);
+    }
+
+    return isQuestionCompleted(4) && isQuestionCompleted(5) && isQuestionCompleted(6);
+}
+
+function checkStepThree() {
+    if (isQuestionCompleted(7)) enableStepControls(3);
+    else disableStepControls(3);
+        
+    return isQuestionCompleted(7);
 }
 
 (function($) {
-    onChangeStep(following => {
-        if (0 < following && following <= 3) {
-            updateProgress(following);
-            showNextStep(following);
-        } else console.log(following);
-    });
+    // Next button handler
+    let nextButtons = $("button.step-control.next");
+    nextButtons.on("click", event => {
+        let button = $(event.target);
+        let nextStep = button.attr("data-next");
 
-    onOptionSelected(1, first => {
-        setQuestionEnabled(2, first);
-
-        onOptionSelected(2, second => {
-            setQuestionEnabled(3, second);
-
-            onOptionSelected(3, third => allowsToContinue(1, third));
-            if (!second) allowsToContinue(1, false);
-        });
-
-        if (!first) {
-            setQuestionEnabled(3, false);
-            allowsToContinue(1, false);
+        if (nextStep === "2" && checkStepOne()) {
+            showNextStep(nextStep);
+            enableQuestion(4);
+        } else if (nextStep === "3" && checkStepTwo()) {
+            showNextStep(nextStep);
+            enableQuestion(7);
         }
-    });
+    })
 
-    onFieldFilled(4, fourth => {
-        setQuestionEnabled(5, fourth);
 
-        onFieldFilled(5, fiveth => {
-            setQuestionEnabled(6, fiveth);
-    
-            onFieldFilled(6, sixth => {
-                allowsToContinue(2, sixth)
-                console.log(sixth)
-            });
-            if (!fiveth) allowsToContinue(2, false);
-        });
+    // Set step 1 validators by question number
+    optionsValidator(1);
+    optionsValidator(2);
+    selectionValidator(3);
+    numberValidator(4);
+    numberValidator(5);
+    numberValidator(6);
+    emailValidator(7);
 
-        if (!fourth) {
-            setQuestionEnabled(6, false);
-            allowsToContinue(2, false);
-        }
-    });
+    // Watch for step 1
+    let options = $(".step[data-step=\"1\"] input, .step[data-step=\"1\"] .option-input");
+    options.on("input", () => checkStepOne());
+
+    // Watch for step 2
+    let inputs = $(".step[data-step=\"2\"] input");
+    inputs.on("input", () => checkStepTwo());
+
+    // Watch for step 2
+    let lasts = $(".step[data-step=\"3\"] input");
+    lasts.on("input", () => checkStepThree());
 
 })(jQuery);
